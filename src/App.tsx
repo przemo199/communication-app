8e9c6c46d78f717b8f41557d161fc49130fa30c9
AdamTadeusz <adam.tomaszewski@hotmail.co.uk>
2021-06-13 11:10:27 +0100

    Added a Clock. Hopefully the changes to package-lock.json dont fuck anything up.

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import ClockInterface from './interfaces/ClockInterface';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+          <Clock />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -19,8 +22,56 @@ function App() {
           Learn React
         </a>
       </header>
+      <div>
+        <h1>Welcome to AnonComms</h1>
+          <p>Use this app to communicate with other anons via a peer-to-peer connection</p>
+          <p>This is another sentence</p>
+      </div>
     </div>
   );
+}
+
+interface DisplayClockInterface{
+    date: Date;
+}
+
+function FormattedDate(props:DisplayClockInterface) {
+    return <h2>It is {props.date.toLocaleTimeString()}.</h2>;
+}
+
+class Clock extends React.Component<any, any> {
+    private timerID: NodeJS.Timeout;
+    constructor(props:ClockInterface) {
+        super(props);
+        this.state = {date: new Date()};
+        this.timerID = setInterval(()=> this.tick(), 1000);
+    }
+
+    componentDidMount() {
+        clearInterval(this.timerID);
+        this.timerID = setInterval(
+            () => this.tick(),
+            1000
+        );
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    tick() {
+        this.setState({
+            date: new Date()
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <FormattedDate date={this.state.date} />
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/interfaces/ClockInterface.ts
@@ -0,0 +1,3 @@
+export default interface ClockInterface{
+    timerID: NodeJS.Timeout;
+}
