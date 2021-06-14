a3a9c975cf70c24eae4c08ca119c17970f1e657d
Przemo199 <przemo199@onet.pl>
2021-06-14 22:48:30 +0100

    Attempting to fix heroku deploy

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,17 +1,22 @@
-import * as path from "path";
-import {fileURLToPath} from "url";
-import express from "express";
+import * as path from 'path';
+import {fileURLToPath} from 'url';
+import express from 'express';
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-app.use(express.static(path.join(__dirname, "..", "build")));
+app.use(express.static(path.join(__dirname, '..', 'build')));
 //app.use(express.static("../public"));
 
 app.use((req, res, next) => {
-    res.sendFile(path.join(__dirname, "..", "build", "index.html"));
+    res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
 });
 
-app.listen(process.env.PORT || 5000, () => {
-    console.log("server started on port 5000");
+app.get('/works', (req, res) => {
+    res.send('Works');
+});
+
+app.listen(PORT, () => {
+    console.log('server started on port 5000');
 });
