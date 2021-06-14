dd02fb260ad9d5f7b4fd1051244d5d6de99afa62
Przemo199 <przemo199@onet.pl>
2021-06-14 23:19:29 +0100

    Figuring out routing on heroku

--- src/server/index.ts
+++ src/server/index.ts
@@ -9,14 +9,14 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 app.use(express.static(path.join(__dirname, '..', 'build')));
 //app.use(express.static("../public"));
 
-app.use((req, res, next) => {
+// app.use((req, res, next) => {
+//     res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
+// });
+
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
 });
 
-app.get('/works', (req, res) => {
-    res.send('Works');
-});
-
 app.listen(PORT, () => {
-    console.log('server started on port 5000');
+    console.log(`server started on port ${PORT}`);
 });
